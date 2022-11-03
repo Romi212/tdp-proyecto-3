931195a13ac9548fd24a79085d60a83a522551e9
martoAs <martinaasteasuain22@gmail.com>
2022-11-03 09:21:41 -0300

    solgrafico?

---
+++ src/Logica/Entidades/SolGrafico.java
@@ -0,0 +1,18 @@
+package Logica.Entidades;
+
+import javax.swing.JLabel;
+
+public class SolGrafico {
+	private static SolGrafico instancia;
+	private String imagen;
+	
+	private SolGrafico() {
+		//imagen = 
+	}
+	
+	public SolGrafico getInstance() {
+		if(instancia==null)
+			instancia = new SolGrafico();
+		return instancia;
+	}
+}
